5b7f3b79d0aa02f1fd0ba8d4a6951bbe4eb44b10
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-06-13 15:24:10 +0200

    context adnotacje

---
+++ src/main/java/pl/sda/Book.java
@@ -0,0 +1,11 @@
+package pl.sda;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class Book {
+
+    private String name;
+}
--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -3,10 +3,14 @@ package pl.sda;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("context.xml");
+
+        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
     }
 }
